feedc83702c8d1a1e85717c4d58bb55b9a654731
Mateusz "Szezjo" Groblewski <szezjo@gmail.com>
2023-05-02 13:51:41 +0200

    change selectinput

--- src/views/SongSelect/SelectInput.tsx
+++ src/views/SongSelect/SelectInput.tsx
@@ -1,19 +1,48 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
 
 type SelectInputProps = {
   id: string,
+  mobileLabel: string,
   options: {displayName: string, value: string}[],
   selectedOption: string,
   setSelectedOption: (option: string) => void,
 }
 
-const SelectInput = ({id, options, selectedOption, setSelectedOption} : SelectInputProps) => {
+const SelectInput = ({id, mobileLabel, options, selectedOption, setSelectedOption} : SelectInputProps) => {
+  const [selectorWidth, setSelectorWidth] = useState({width: 0});
+  const [selectedOptionText, setSelectedOptionText] = useState(options[0].displayName);
+  const tempSelectorRef = useRef<HTMLSelectElement>(null);
+  const [displayTempElement, setDisplayTempElement] = useState(true);
+
+  const setOption = (option: string) => {
+    const optionData = options.find((e) => e.value===option);
+    setSelectedOption(option);
+    setSelectedOptionText(optionData ? optionData.displayName : "");
+  }
+
+  useLayoutEffect(() => {
+    if (tempSelectorRef && tempSelectorRef.current) {
+      if (displayTempElement) setSelectorWidth({width: tempSelectorRef.current.offsetWidth});
+      setDisplayTempElement(false);
+    } 
+  }, [tempSelectorRef.current, displayTempElement])
+
+  useEffect(() => {
+    setDisplayTempElement(true);
+  }, [selectedOption])
+
     return (
-      <select value={selectedOption} onChange={(e) => setSelectedOption(e.target.value)} id={id} className="block py-2.5 px-0 text-sm bg-transparent md:bg-gray-800 text-white">
-        {options.map((e) => (
-          <option value={e.value} key={e.value}>{e.displayName}</option>
-        ))}
-      </select>
+      <div className="flex py-2.5 bg-gray-700 w-full rounded-lg justify-center md:justify-normal md:w-fit md:bg-transparent">
+        <span className="text-sm text-white font-bold md:hidden">{mobileLabel}:</span>
+        <select value={selectedOption} onChange={(e) => setOption(e.target.value)} id={id} className={`block outline-none px-0 text-sm bg-gray-700 md:bg-gray-800 text-white`} style={selectorWidth}>
+          {options.map((e) => (
+            <option value={e.value} key={e.value}>{e.displayName}</option>
+          ))}
+        </select>
+        <select id="width_tmp_select" style={{display: displayTempElement ? 'block' : 'none'}} ref={tempSelectorRef}>
+          <option id="width_tmp_option">{selectedOptionText}</option>
+        </select>
+      </div>
     )
 }
 
--- src/views/SongSelect/SongsWindow.tsx
+++ src/views/SongSelect/SongsWindow.tsx
@@ -10,6 +10,14 @@ const SongsWindow = () => {
     ]
     const [sortOption, setSortOption] = useState(sortOptions[0].value);
 
+    const filterOptions = [
+        {displayName: "Wszystkie", value: "all"},
+        {displayName: "Tylko z trybem rankingowym", value: "scoreModeOnly"},
+        {displayName: "Tylko ulubione", value: "favouritesOnly"}
+    ]
+
+    const [filterOption, setFilterOption] = useState(filterOptions[0].value)
+
     const address = import.meta.env.VITE_SERVER_ADDRESS;
 
     const {
@@ -21,8 +29,9 @@ const SongsWindow = () => {
 
     return (
         <div className="h-[calc(100vh-7rem)] overflow-y-hidden flex flex-col md:p-4 md:bg-gray-800 md:border md:border-gray-700 md:rounded-lg md:shadow lg:col-span-2">
-            <div className="rounded-lg flex m-2 bg-gray-700 md:bg-gray-800 justify-center md:justify-end md:m-0">
-                <SelectInput id="sort-select" options={sortOptions} selectedOption={sortOption} setSelectedOption={setSortOption} />
+            <div className="rounded-lg flex flex-col space-y-4 m-2 py-2 items-center md:space-y-0 md:justify-end md:m-0 md:flex-row md:space-x-6">
+                <SelectInput id="filter-select" mobileLabel="Pokaż" options={filterOptions} selectedOption={filterOption} setSelectedOption={setFilterOption} />
+                <SelectInput id="sort-select" mobileLabel="Sortuj" options={sortOptions} selectedOption={sortOption} setSelectedOption={setSortOption} />
             </div>
             <div className="md:grid md:grid-cols-2 overflow-y-auto max-h-[calc(100vh-10rem)]">
             {isSuccess && data.map((e) => (
